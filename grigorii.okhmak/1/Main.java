696ad7a709f389f712c744b5b9b3b86d6b139553
MrScriptMaster <ohmak88@yandex.ru>
2020-04-28 00:46:45 +0300

    Added task 1

---
+++ grigorii.okhmak/1/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Error: need two numeric arguments");
+            System.exit(1);
+        }
+        Integer number1 = 0, number2 = 0;
+        try {
+            number1 = Integer.parseInt(args[0]);
+            number2 = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        if (number1 == number2) {
+            System.out.println("Numbers are equal");
+        } else if (number1 < number2) {
+            System.out.println("Number " + number1 + " is less than " + number2);
+        } else {
+            System.out.println("Number " + number1 + " is greater than " + number2);
+        }
+        System.out.println("Sum of numbers is " + (number1 + number2));
+    }
+}
