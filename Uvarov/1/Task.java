afa82f32116cab7112d8b15c86213f874051f867
Apollinary <apollinariiuvarov@mail.ru>
2020-04-28 21:30:37 +0300

    Update Task.java
    Used Eglish symbols to improve readability.

--- Uvarov/1/Task.java
+++ Uvarov/1/Task.java
@@ -3,14 +3,15 @@ package at;
 public class Task {
 
 	public static void main(String args[]) {
+		
 		int firstNumber = Integer.valueOf(args[0]);
 		int secondNumber = Integer.valueOf(args[1]);
 		
 		if (firstNumber > secondNumber) {
-			System.out.println(firstNumber + " больше " + secondNumber);
+			System.out.println(firstNumber + " is more then " + secondNumber);
 		}
 		else {
-			System.out.println(firstNumber + " не больше " + secondNumber);
+			System.out.println(firstNumber + " is no more then " + secondNumber);
 		}
 		System.out.println(firstNumber + secondNumber);
 	}
