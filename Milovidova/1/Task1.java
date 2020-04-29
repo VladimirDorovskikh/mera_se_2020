51f27cbf7056b8d63d75a2baff4a6466e1712f88
SvMil <64522582+SvMil@users.noreply.github.com>
2020-04-29 11:09:28 +0300

    Create Task1.java
    JavaSE_Lesson1_Ex1

---
+++ Milovidova/1/Task1.java
@@ -0,0 +1,30 @@
+
+public class task1 {
+	public static void main (String args[]){
+
+		int var1 = 43;
+		int var2 = 45;
+		
+			
+		if (var1 > var2)
+		{
+			System.out.println("var1 is more then var2" );
+		} 
+		else if (var1 < var2)
+		{
+			System.out.println("var2(" + var2 + ") is more then var1(" + var1 + ")" );
+		} 
+		else if (var1 == var2)
+		{
+			System.out.println("var2 equals var1" );
+		}
+		else
+		{
+			System.out.println("Check the type of variable values" );
+		}
+		
+		System.out.println("The sum of the values of two variables being compared (var1 + var2) is " + (var1 + var2) );
+		
+				}
+}
+
