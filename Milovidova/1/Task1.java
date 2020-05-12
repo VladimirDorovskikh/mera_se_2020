fa70b7f983ad5641b9018988d9727207ccd977fc
SvMil <64522582+SvMil@users.noreply.github.com>
2020-05-12 10:46:58 +0300

    Fix Lecture1Task1

--- Milovidova/1/Task1.java
+++ Milovidova/1/Task1.java
@@ -1,11 +1,10 @@
 
-public class task1 {
+public class Main {
 	public static void main (String args[]){
 
-		int var1 = 43;
-		int var2 = 45;
-		
-			
+		int var1 = 47;
+		int var2 = 47;
+					
 		if (var1 > var2)
 		{
 			System.out.println("var1 is more then var2" );
@@ -18,13 +17,11 @@ public class task1 {
 		{
 			System.out.println("var2 equals var1" );
 		}
-		else
-		{
-			System.out.println("Check the type of variable values" );
-		}
+		
 		
 		System.out.println("The sum of the values of two variables being compared (var1 + var2) is " + (var1 + var2) );
-		
-				}
+				
+	}
+	
 }
 
