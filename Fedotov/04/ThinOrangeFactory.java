60f38f736b389898e17ddc2533f32e7143091296
108rfe <33877762+108rfe@users.noreply.github.com>
2020-05-13 19:52:51 +0300

    Update ThinOrangeFactory.java

--- Fedotov/04/ThinOrangeFactory.java
+++ Fedotov/04/ThinOrangeFactory.java
@@ -1,9 +1,13 @@
 package com.mera.training.task04;
 
 public class ThinOrangeFactory extends AbstractFabric {
-    private final int skinThickness = 1 + (int) (Math.random() * ((5 - 1) + 1)); // From 1 to 5
+    public Fruit makeFruit() {
 
-    public String makeFruit() {
-        return "Orange" + ", skinThickness: " + skinThickness;
+        Orange fruit;
+        do {
+            fruit = new Orange();
+        }
+        while ((fruit.getSkinThickness() < 1) || (fruit.getSkinThickness() > 5));
+        return fruit;
     }
 }
