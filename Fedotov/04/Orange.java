b4ac28207ec5133096177a89856eff00347405cc
108rfe <33877762+108rfe@users.noreply.github.com>
2020-05-13 09:09:21 +0300

    Create Orange.java

---
+++ Fedotov/04/Orange.java
@@ -0,0 +1,18 @@
+package com.mera.training.task04;
+
+public class Orange extends Fruit {
+    private int skinThickness = super.getFromInterval(0, 20); // From 0 to 20
+
+    public Orange() {
+        super();
+    }
+
+    private int getSkinThickness() {
+        return this.skinThickness;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + ", Skin Thickness: " + getSkinThickness() + ", Weight: " + super.getWeight();
+    }
+}
