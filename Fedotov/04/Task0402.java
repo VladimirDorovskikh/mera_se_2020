dab10e9cdf9cb1301d6079d68b1c8e3c2bfa4e29
108rfe <33877762+108rfe@users.noreply.github.com>
2020-05-13 19:52:17 +0300

    Update Task0402.java

--- Fedotov/04/Task0402.java
+++ Fedotov/04/Task0402.java
@@ -5,66 +5,37 @@ import java.util.Random;
 public class Task0402 {
 
     public static void main(String[] args) {
-        {
-            System.out.println("Фруктовая абстрактность: Случайные фабрики одного типа");
 
-            final int factoryTypes = 4;
-            final int factoryNumbers = 10;
+        System.out.println("Фруктовая абстрактность");
 
-            int randomFactory = new Random().nextInt(factoryTypes);
-            AbstractFabric[] factories = new AbstractFabric[factoryNumbers];
+        final int factoryTypes = 4;
+        final int factoryCount = 10;
+        final int fruitCount = 20;
 
-            for (int i = 0; i < factoryNumbers; ++i) {
-                switch (randomFactory) {
-                    case 0:
-                        factories[i] = new ThinOrangeFactory();
-                        break;
-                    case 1:
-                        factories[i] = new RedApplesFactory();
-                        break;
-                    case 2:
-                        factories[i] = new GreenApplesFactory();
-                        break;
-                    default:
-                        factories[i] = new LongPinepplesFactory();
-                }
-                System.out.println(factories[i].makeFruit());
+        int randomFactory = new Random().nextInt(factoryTypes);
+        AbstractFabric[] factories = new AbstractFabric[factoryCount];
+        Fruit[] fruit = new Fruit[fruitCount];
+
+        for (int i = 0; i < factoryCount; ++i) {
+            switch (randomFactory) {
+                case 0:
+                    factories[i] = new ThinOrangeFactory();
+                    break;
+                case 1:
+                    factories[i] = new RedApplesFactory();
+                    break;
+                case 2:
+                    factories[i] = new GreenApplesFactory();
+                    break;
+                default:
+                    factories[i] = new LongPinepplesFactory();
             }
+            System.out.println(factories[i].makeFruit());
+            fruit[i * 2] = factories[i].makeFruit();
+            fruit[i * 2 + 1] = factories[i].makeFruit();
         }
-
-        {
-            System.out.println("\nФруктовая абстрактность: По 2 фрукта от одной фабрики");
-
-            final int factoryNumbers = 4;
-            final int fruitNumbers = 20;
-
-            AbstractFabric[][] factories = new AbstractFabric[fruitNumbers][2];
-
-            int i = 0;
-            while (i < fruitNumbers) {
-                for (int j = 0; j < factoryNumbers; ++j) {
-                    switch (j) {
-                        case 0:
-                            factories[i][0] = new ThinOrangeFactory();
-                            factories[i++][1] = new ThinOrangeFactory();
-                            break;
-                        case 1:
-                            factories[i][0] = new RedApplesFactory();
-                            factories[i++][1] = new RedApplesFactory();
-                            break;
-                        case 2:
-                            factories[i][0] = new GreenApplesFactory();
-                            factories[i++][1] = new GreenApplesFactory();
-                            break;
-                        default:
-                            factories[i][0] = new LongPinepplesFactory();
-                            factories[i++][1] = new LongPinepplesFactory();
-                    }
-                }
-            }
-            for (int k = 0; k < fruitNumbers; ++k) {
-                System.out.println(factories[k][0].makeFruit() + ", " + factories[k][1].makeFruit());
-            }
+        for (Fruit fruitObj : fruit) {
+            System.out.println(fruitObj.toString());
         }
     }
 }
