6ca041fd908bda0af519ff407409a15ccc581fc6
Aleksey Pradedov <apradedov@gmail.com>
2020-06-01 00:48:25 +0300

    Lab8

---
+++ Pradedov/8/src/com/pradedov/lab8/serialization/JsonName.java
@@ -0,0 +1,12 @@
+package com.pradedov.lab8.serialization;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface JsonName {
+    String key();
+}
