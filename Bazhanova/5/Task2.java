3d8612284f769dc503a783645cc3ee7b1fc6bad0
mila2592 <arksinus-bmw@yandex.ru>
2020-06-01 23:18:04 +0300

    Update Task2.java

--- Bazhanova/5/Task2.java
+++ Bazhanova/5/Task2.java
@@ -1,92 +1,109 @@
 import java.util.Random;
 
 
-public class Task2{
-      
+public class Task2 {
+
     public static void main(String[] args) {
 
         int maxName = 5;
         int numSeasons = 4;
-         
-        String[] firstNames  = new String[] {"Роман", "Тимофей", "Владимир", "Афанасий", "Алексей"};
 
-        Season[] seasons  = new Season[] {Season.Зима, Season.Весна, Season.Лето, Season.Осень};
-        
+        String[] firstNames = new String[] {
+            "Роман",
+            "Тимофей",
+            "Владимир",
+            "Афанасий",
+            "Алексей"
+        };
+
+        Season[] seasons = new Season[] {
+            Season.ЗИМА, Season.ВЕСНА, Season.ЛЕТО, Season.ОСЕНЬ
+        };
+
         int numOfPersons = 10;
-        int name=0, season=0;
-        
-        Random rnd = new Random(System.currentTimeMillis());
-        
-        Person[] persons  = new Person[numOfPersons]; 
-        
-        for(int i=0;i<numOfPersons;i++) {
-        name = rnd.nextInt(maxName);
-        season = rnd.nextInt(numSeasons);
-        persons[i] = new Person(firstNames[name], seasons[season]);
-          System.out.printf("Человек №%d - %s (%s)\n", i, persons[i], persons[i].favoriteSeason.getSeason());
+
+
+        Random rnd = new Random();
+
+        Person[] persons = new Person[numOfPersons];
+
+        for (int i = 0; i < numOfPersons; i++) {
+            int name = rnd.nextInt(maxName);
+            int season = rnd.nextInt(numSeasons);
+            persons[i] = new Person(firstNames[name], seasons[season]);
+            System.out.printf("Человек №%d - %s (%s)\n", i, persons[i], persons[i].favoriteSeason.getSeasonGame());
         }
-        
+
     }
 }
 
 
 enum Games {
-  
-    snow("снежки"), 
-    boats("запуск корабликов по ручьям"), 
-    berries("собирание и поедание ягод"), 
-    plash("измерение глубины луж методом \"сапога\"");
-        
+
+    SNOW("снежки"),
+    BOATS("запуск корабликов по ручьям"),
+    BERRIES("собирание и поедание ягод"),
+    PLASH("измерение глубины луж методом \"сапога\"");
+
     private String game;
-        
-        
-    Games(String game){
+
+
+    Games(String game) {
         this.game = game;
     }
-    
-    public String getGame(){ return game;}
+
+    public String getGame() {
+        return game;
+    }
+
+    @Override
+    public String toString() {
+        return game;
+    }
 }
 
 enum Season {
-  
-    Зима(Games.snow.getGame()),
-    Весна(Games.boats.getGame()),
-    Лето(Games.berries.getGame()),
-    Осень(Games.plash.getGame());
-    
-    private String season;
-        
-        
-    Season(String season){
-        this.season = season;
+
+    ЗИМА(Games.SNOW),
+    ВЕСНА(Games.BOATS),
+    ЛЕТО(Games.BERRIES),
+    ОСЕНЬ(Games.PLASH);
+
+    private Games seasonGame;
+
+
+    Season(Games seasonGame) {
+        this.seasonGame = seasonGame;
     }
-    
-    public String getSeason(){ return season;}
-    
+
+    public String getSeasonGame() {
+        return seasonGame.toString();
+    }
+
 }
 
-class Person{
-     
-    String name;    // имя
-    Season favoriteSeason;    // любимый сезон
+class Person {
+
+    String name; // имя
+    Season favoriteSeason; // любимый сезон
 
     Person(String name, Season favoriteSeason) {
         this.name = name;
         this.favoriteSeason = favoriteSeason;
     }
 
-    
-    public String getFavoriteSeason() {
-       return favoriteSeason.getSeason();
+
+    public Season getFavoriteSeason() {
+        return favoriteSeason;
     }
-   
-    public String getName () {
-       return name;
+
+    public String getName() {
+        return name;
     }
-   
+
     public String toString() {
         return (name + ", любимое время года - " + favoriteSeason);
-        
-   }
-   
+
+    }
+
 }
