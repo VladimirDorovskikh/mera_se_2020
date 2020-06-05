c4f532cbb27550b118b1d23d2fdc7b13155bb979
OlgaNaidenova <64428566+OlgaNaidenova@users.noreply.github.com>
2020-06-05 23:22:21 +0300

    Update MyFileVisitor.java

--- Naidenova/9/MyFileVisitor.java
+++ Naidenova/9/MyFileVisitor.java
@@ -1,13 +1,16 @@
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyFileVisitor extends SimpleFileVisitor<Path> {
-    String partOfName;
-    String partOfContent;
+    String partOfName = "File_";
+    public static Map<File, Long> foundFiles = new HashMap<File, Long>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
@@ -15,15 +18,21 @@ public class MyFileVisitor extends SimpleFileVisitor<Path> {
         boolean containsName = true;
         if (partOfName != null && !file.getFileName().toString().contains(partOfName))
             containsName = false;
-
-        String content = new String(Files.readAllBytes(file));
-        boolean containsContent = true;
-        if (partOfContent != null && !content.contains(partOfContent))
-            containsContent = false;
-
-        System.out.println(file.getParent() + "\\" + file.getFileName());
+        else {
+            System.out.println(file.getParent() + "\\" + file.getFileName());
+            File f = new File(file.getParent() + "\\" + file.getFileName());
+            System.out.println(getFileSize(f)+" bytes");
+            foundFiles.put(f, getFileSize(f));
+        }
 
         return FileVisitResult.CONTINUE;
     }
 
+    public static Map<File, Long> getFoundFiles() {
+        return foundFiles;
+    }
+
+    private static Long getFileSize(File file){
+        return file.length();
+    }
 }
