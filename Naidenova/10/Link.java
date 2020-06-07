c567277730cf35ee8955cd7e53145262db4f74d5
OlgaNaidenova <64428566+OlgaNaidenova@users.noreply.github.com>
2020-06-08 02:20:11 +0300

    Create Link.java

---
+++ Naidenova/10/Link.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.net.URL;
+
+public class Link {
+    private static int size;
+
+    public static void oneLink(String url) throws IOException, InterruptedException {
+
+        Map<String, String> URLsContent = new ConcurrentHashMap<>();
+        long lStartTime = System.currentTimeMillis();
+        try {
+            URL site = new URL(url);
+            InputStream inputStream = (java.io.InputStream) site.openStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                URLsContent.put(url, line);
+                size += line.length();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long lEndTime = System.currentTimeMillis();
+        long output = lEndTime - lStartTime;
+    }
+
+    public static int getSize() {
+        return size;
+    }
+
+}
