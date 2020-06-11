ec481a469aea118c28fa37fcb94d32e6e20eb822
Dectlab <dectlab@ascom-ws.com>
2020-06-11 17:29:44 +0300

    Add lesson11

---
+++ Evgenii.Volosenkov/Lesson11/com/mera/volosenkov/lesson11/Calculator/IOperation.java
@@ -0,0 +1,5 @@
+package com.mera.volosenkov.lesson11.Calculator;
+
+public interface IOperation {
+    Double calculate(Double val1, Double val2);
+}
---
+++ Evgenii.Volosenkov/Lesson11/com/mera/volosenkov/lesson11/Person.java
@@ -0,0 +1,27 @@
+package com.mera.volosenkov.lesson11;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
